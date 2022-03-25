719345303b1aa2eb2775289db370d4a4ebc4e7ac
elviredev <sandrine.rodriguez@hotmail.com>
2022-03-25 16:07:47 +0100

    add seeder - geospatial - filtering - pagination - courses - populate virtuals and cascade delete - crud courses - aggregate for calculate average course cost

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// @ts-nocheck
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
 // Load env vars
@@ -12,6 +15,7 @@ connectDB()
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
 
 const app = express()
 
@@ -23,8 +27,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-// Mount routers
+// File uploading
+app.use(fileupload())
+
+// Utiliser routers
 app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
